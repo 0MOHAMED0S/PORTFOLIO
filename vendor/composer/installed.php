cd98814f7cdb7131eea52c1da3adac871cd959ce
mohamed <msa0back@gmail.com>
2025-01-06 23:11:09 +0200

    save

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elgla/i-portfolio',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e684d24245d15c8b4c48059fcc08cf63456c4c3a',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'elgla/i-portfolio' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e684d24245d15c8b4c48059fcc08cf63456c4c3a',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
